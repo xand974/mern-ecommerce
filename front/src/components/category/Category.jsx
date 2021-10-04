9311cdb193b35ad7c8b4890344b2f320c357991f
Alexandre <xand974@gmail.com>
2021-10-04 12:44:11 +0200

    front : slider design

--- front/src/components/category/Category.jsx
+++ front/src/components/category/Category.jsx
@@ -9,18 +9,31 @@ export default function Category() {
             src="https://images.unsplash.com/photo-1600328759671-85927887458d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
             alt=""
           />
+
+          <div className="category__options">
+            <h4>For the Woman</h4>
+            <span>Voir plus</span>
+          </div>
         </div>
         <div className="infos">
           <img
             src="https://images.unsplash.com/photo-1571945153237-4929e783af4a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
             alt=""
           />
+          <div className="category__options">
+            <h4>For the Men</h4>
+            <span>Voir plus</span>
+          </div>
         </div>
         <div className="infos">
           <img
             src="https://images.unsplash.com/photo-1508427953056-b00b8d78ebf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt=""
           />
+          <div className="category__options">
+            <h4>White Style!</h4>
+            <span>Voir plus</span>
+          </div>
         </div>
       </div>
     </section>
--- front/src/components/slider/Slider.jsx
+++ front/src/components/slider/Slider.jsx
@@ -1,5 +1,42 @@
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 import "./slider.scss";
 
 export default function Slider() {
-  return <div className="slider">slider</div>;
+  return (
+    <div className="slider">
+      <div className="wrapper">
+        <ArrowBackIos className="arrow left" />
+        <div className="slider__images">
+          <div className="slider__container">
+            <img
+              src="https://images.unsplash.com/photo-1599255068390-206e0d068539?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=465&q=80"
+              alt=""
+            />
+          </div>
+
+          <div className="slider__container">
+            <img
+              src="https://images.unsplash.com/photo-1523647341782-d761bce0004c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+              alt=""
+            />
+          </div>
+
+          <div className="slider__container">
+            <img
+              src="https://images.unsplash.com/photo-1505308088817-f1e3cb2a2f63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+              alt=""
+            />
+          </div>
+
+          <div className="slider__container">
+            <img
+              src="https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <ArrowForwardIos className="arrow right" />
+      </div>
+    </div>
+  );
 }
