e5a3b65cae2dd6a6ffc30d4b97c9fc9786c986f1
Alexandre <xand974@gmail.com>
2021-10-12 12:59:09 +0200

    front : setup cart slice

---
+++ front/src/redux/cartSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    products: [],
+    quantity: 0,
+    total: 0,
+  },
+  reducers: {
+    //ajouter produit
+    //supprimer produit
+  },
+});
