a80cd7703c722f767e61333bece9259a0023f8cd
Alexandre <xand974@gmail.com>
2021-10-12 22:47:30 +0200

    back : fix stripe typos

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 router.post("/payment", (req, res) => {
-  stripe.charge.create(
+  stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
