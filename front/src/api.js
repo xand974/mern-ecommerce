e5042b88022120ebfd101a379dabb142a9fda49a
xand974 <xand974@gmail.com>
2021-10-19 21:39:46 +0200

    front : handle missing token

--- front/src/api.js
+++ front/src/api.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 
 const BaseURL = "http://localhost:3001/api/v1";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
-  ?.currentUser?.accessToken;
+let TOKEN;
+
+//#region CHECK TOKEN
+const checkLocalStorage = () => {
+  if (JSON.parse(localStorage.getItem("persist:root")) === null) {
+    TOKEN = "";
+  } else {
+    TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
+      .currentUser?.accessToken;
+  }
+};
+
+//#endregion
+
+checkLocalStorage();
 
 export const publicRequest = axios.create({ baseURL: BaseURL });
 export const privateRequest = axios.create({
