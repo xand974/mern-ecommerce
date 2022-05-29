2e424961a4a72453becdc6fb07c4cf6a0edbb71e
xand974 <xand974@gmail.com>
2022-05-29 10:02:58 +0200

    use interceptors

--- api/middlewares/verify.js
+++ api/middlewares/verify.js
@@ -1,26 +1,32 @@
 const jwt = require("jsonwebtoken");
 module.exports = {
   verifyToken: (req, res, next) => {
-    const token = req.headers.token;
-    if (!token) return res.status(401).json("you are not authentificated");
+    const tokenFromHeader = req.headers.authorization;
+    if (!tokenFromHeader)
+      return res.status(401).json("you are not authentificated");
+    const token = tokenFromHeader.split(" ")[1];
 
     jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, payload) => {
       if (err) return res.status(403).json("token is not valid");
 
       req.user = payload;
-      next();
+      return next();
     });
   },
   verifyIsAdmin: (req, res, next) => {
     if (req.user.isAdmin) {
-      next();
+      return next();
     } else {
       return res.status(401).json("you are not allowed to do this");
     }
   },
   verifyUserOrIsAdmin: (req, res, next) => {
-    if (req.user._id === req.params.id || req.user.isAdmin) {
-      next();
+    if (
+      req.user._id === req.params.id ||
+      req.user.isAdmin ||
+      req.user._id === req.body.userId
+    ) {
+      return next();
     } else {
       return res.status(401).json("you are not allowed to do this");
     }
--- front/src/api.js
+++ front/src/api.js
@@ -1,26 +1,21 @@
 import axios from "axios";
+import { store } from "redux/store";
 
-const BaseURL = "http://localhost:3001/api/v1";
-let TOKEN;
-
-//#region CHECK TOKEN
-const checkLocalStorage = () => {
-  if (JSON.parse(localStorage.getItem("persist:root")) === null) {
-    TOKEN = "";
-  } else {
-    TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
-      .currentUser?.accessToken;
-  }
-};
-
-//#endregion
-
-checkLocalStorage();
+const BaseURL = process.env.REACT_APP_API_URL;
 
 export const publicRequest = axios.create({ baseURL: BaseURL });
-export const privateRequest = axios.create({
+const privateRequest = axios.create({
   baseURL: BaseURL,
-  headers: {
-    token: TOKEN,
-  },
 });
+
+privateRequest.interceptors.request.use(
+  (config) => {
+    const TOKEN = store.getState().user?.currentUser?.accessToken;
+    const auth = TOKEN ? `Bearer ${TOKEN}` : "";
+    config.headers.common["authorization"] = auth;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export { privateRequest };
